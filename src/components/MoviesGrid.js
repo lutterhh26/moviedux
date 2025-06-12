8cd274c27ef7224f8a141d498ee7d838afbfb62d
Helbert <your.email@example.com>
2025-06-12 16:28:52 -0400

    Creating the Movie Card

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,40 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
 
-export default function MoviesGrid () {
-    
-    const [movies, setMovies] = useState([]);
-    
-    const m = ["a", "b", "c"]
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
+  const m = ["a", "b", "c"];
 
-        fetch("movies.json")
-        .then(response => response.json())
-        .then(data => setMovies(data))
-        
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
 
-    },[]);
-
-    return (
-        <div className = 'movies-grid'>
-
-        {
-            movies.map(movie =>(
-                <div key={movie.id} className = 'movie-card'>
-                    <img src = {`images/${movie.image}`} alt = {movie.title} />
-                    <div className = 'movie-card' >
-                        <h3 className = 'movie-card-info'>{movie.title}</h3>
-                        <p className = 'movie-card-genre'>{movie.genre}</p>
-                        <p className = 'movie-card-rating'>{movie.rating}</p>      
-                    </div> 
-
-                </div>
-            ))
-
-        }
-
-
-        </div>
-    );
+  return (
+    <div className="movies-grid">
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.id}></MovieCard>
+      ))}
+    </div>
+  );
 }
