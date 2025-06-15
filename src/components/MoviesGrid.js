f1be5c673c4505b5088987fe62d75d51f2bd1ad9
Helbert <your.email@example.com>
2025-06-15 11:31:07 -0400

    Making a Filter

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,7 +5,9 @@ import MovieCard from "./MovieCard";
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const m = ["a", "b", "c"];
+
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
 
   useEffect(() => {
     fetch("movies.json")
@@ -30,6 +32,30 @@ export default function MoviesGrid() {
         value={searchTerm}
         onChange={handleSearchChange}
       />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select className="filter-dropdown">
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
+
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select className="filter-dropdown">
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard movie={movie} key={movie.id}></MovieCard>
