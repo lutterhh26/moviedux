a97d9c6ec66924304f39d96a90479ff419b3db55
Helbert <your.email@example.com>
2025-06-17 13:41:25 -0400

    Lifting the movie state

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
 import Watchlist from "./components/Watchlist";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
   return (
     <div className="App">
       <div className="container">
@@ -27,7 +34,7 @@ function App() {
           </nav>
 
           <Routes>
-            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/" element={<MoviesGrid movies={movies} />}></Route>
             <Route path="/watchlist" element={<Watchlist />}></Route>
           </Routes>
         </Router>
